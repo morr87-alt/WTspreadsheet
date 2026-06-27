94033a133a0ce02875fd0b3553889a2b2d7c0523
morr87-alt <mortojunior87@gmail.com>
2026-06-28 00:17:31 +0100

    2

--- functions/api/listar.js
+++ functions/api/listar.js
@@ -1,1 +1,12 @@
+export async function onRequestGet(context) {
+  const db = context.env.DB;
 
+  const url = new URL(context.request.url);
+  const chave = url.searchParams.get("chave");
+
+  const data = await db.prepare(
+    "SELECT valor FROM conteudo WHERE chave = ?"
+  ).bind(chave).first();
+
+  return Response.json(data || {});
+}
