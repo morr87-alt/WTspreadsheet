4414135ca2befda5d56b2bcab11739ba01a67550
morr87-alt <mortojunior87@gmail.com>
2026-06-28 00:01:31 +0100

    1

--- functions/api/guardar.js
+++ functions/api/guardar.js
@@ -1,14 +1,6 @@
 export async function onRequestPost(context) {
-  const db = context.env.DB;
-
-  const { nome, email } = await context.request.json();
-
-  await db
-    .prepare("INSERT INTO utilizadores (nome, email) VALUES (?, ?)")
-    .bind(nome, email)
-    .run();
-
   return Response.json({
-    sucesso: true
+    dbExiste: !!context.env.DB,
+    keys: Object.keys(context.env)
   });
 }
