f65fdc5c699c18ce2212643b15a64c889e8ea414
lusos <mortojunior87@gmail.com>
2026-06-28 01:10:47 +0100

    bota que tem

---
+++ js/storage.js
@@ -0,0 +1,31 @@
+window.Storage = {
+
+    async load(){
+
+        const data = await API.load();
+
+        window._edDB       = data.missiles   || {};
+        window._skDB       = data.seekers    || {};
+        window.aircraftDB  = data.aircraft   || {};
+        window._customTabs = data.customTabs || [];
+
+        return data;
+    },
+
+    async save(){
+
+        return await API.save({
+
+            missiles: window._edDB,
+
+            seekers: window._skDB,
+
+            aircraft: window.aircraftDB,
+
+            customTabs: window._customTabs
+
+        });
+
+    }
+
+};
